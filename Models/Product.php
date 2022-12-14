ced4a8ae82812cb5e8dabba1c89c579f2819c5dc
“BarrioTropical” <barrio>
2022-12-14 16:21:35 +0100

    models: category & product
    Signed-off-by: “BarrioTropical” <“barrio>

---
+++ Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/Category.php';
+    class Product {
+        public $name;
+        public $price;
+        public $discount;
+        public $category;
+        public $brand;
+        public $image;
+
+        function __construct(string $_name,int $_price, string $_discount, string $_category, string $_brand, string $_image)
+        {
+            $this->name = $_name;
+            $this->price = $_price;
+            $this->discount = $_discount;
+            $this->category = $_category;
+            $this->brand = $_brand;
+            $this->image = $_image;
+        }
+    }
+?>
