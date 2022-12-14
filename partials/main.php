d7b0c0758437330b6b2ce1ba80138544a668ab0e
“BarrioTropical” <barrio>
2022-12-14 17:15:29 +0100

    print card
    Signed-off-by: “BarrioTropical” <“barrio>

--- partials/main.php
+++ partials/main.php
@@ -1,5 +1,158 @@
 <?php
 ?>
 <main class="container">
-Sono Main
+    <h2 class="text-center py-2">Food</h2>
+    <div class="container-fluid">
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product1->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product1->name ?></h5>
+                    <h6><?php echo $product1->category ?></h6>
+                    <p class="card-text"><?php echo $product1->price ?></p>
+                    <p class="card-text"><?php echo $product1->discount ?></p>
+                    <h6><?php echo $product1->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product2->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product2->name ?></h5>
+                    <h6><?php echo $product2->category ?></h6>
+                    <p class="card-text"><?php echo $product2->price ?></p>
+                    <p class="card-text"><?php echo $product2->discount ?></p>
+                    <h6><?php echo $product2->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product3->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product3->name ?></h5>
+                    <h6><?php echo $product3->category ?></h6>
+                    <p class="card-text"><?php echo $product3->price ?></p>
+                    <p class="card-text"><?php echo $product3->discount ?></p>
+                    <h6><?php echo $product3->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <h2 class="text-center py-2">Game</h2>
+    <div class="container-fluid">
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product4->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product4->name ?></h5>
+                    <h6><?php echo $product4->category ?></h6>
+                    <p class="card-text"><?php echo $product4->price ?></p>
+                    <p class="card-text"><?php echo $product4->discount ?></p>
+                    <h6><?php echo $product4->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product5->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product5->name ?></h5>
+                    <h6><?php echo $product5->category ?></h6>
+                    <p class="card-text"><?php echo $product5->price ?></p>
+                    <p class="card-text"><?php echo $product5->discount ?></p>
+                    <h6><?php echo $product5->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product6->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product6->name ?></h5>
+                    <h6><?php echo $product6->category ?></h6>
+                    <p class="card-text"><?php echo $product6->price ?></p>
+                    <p class="card-text"><?php echo $product6->discount ?></p>
+                    <h6><?php echo $product6->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product7->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product7->name ?></h5>
+                    <h6><?php echo $product7->category ?></h6>
+                    <p class="card-text"><?php echo $product7->price ?></p>
+                    <p class="card-text"><?php echo $product7->discount ?></p>
+                    <h6><?php echo $product7->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product8->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product8->name ?></h5>
+                    <h6><?php echo $product8->category ?></h6>
+                    <p class="card-text"><?php echo $product8->price ?></p>
+                    <p class="card-text"><?php echo $product8->discount ?></p>
+                    <h6><?php echo $product8->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mx-auto col-md-3 col-10 mt-5">
+            <img class='mx-auto img-thumbnail'
+                src="<?php echo $product9->image ?>"
+                width="auto" height="auto"/>
+            <div class="card-body text-center mx-auto">
+                <div>
+                    <h5 class="card-title font-weight-bold"><?php echo $product9->name ?></h5>
+                    <h6><?php echo $product9->category ?></h6>
+                    <p class="card-text"><?php echo $product9->price ?></p>
+                    <p class="card-text"><?php echo $product9->discount ?></p>
+                    <h6><?php echo $product9->brand ?></h6>
+                    <a href="#" class="btn cart px-auto">ADD TO CART</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
 </main>
